6812b35bd5bb366d5ded508252f94999b4bcf8bf
mariadpf05 <mariadelpino05@iesamachado.org>
2024-11-06 12:32:16 +0100

    asd

--- 1ºTRIMESTRE/javascript/repaso 2/imc.js
+++ 1ºTRIMESTRE/javascript/repaso 2/imc.js
@@ -5,7 +5,7 @@ function CalcularIMC(){
 
     if (isNaN(peso) || isNaN(alturaCentimetros) || peso <= 0 || alturaCentimetros <= 0) {
         document.getElementById("resultado").innerHTML = "Por favor, ingrese valores válidos de peso y altura.";
-        document.getElementById("recomendacion").innerHTML = "";
+        document.getElementById("comentario").innerHTML = "";
         return;
     }
 
@@ -14,18 +14,18 @@ function CalcularIMC(){
 
     if (IMC < 18.5) {
         document.getElementById("resultado").innerHTML = "Su índice de masa corporal es " + IMC.toFixed(2);
-        document.getElementById("recomendacion").innerHTML = "Su peso es inferior al normal.";
+        document.getElementById("comentario").innerHTML = "Su peso es inferior al normal.";
     } 
     else if (IMC >= 18.5 && IMC <= 24.9) {
         document.getElementById("resultado").innerHTML = "Su índice de masa corporal es " + IMC.toFixed(2);
-        document.getElementById("recomendacion").innerHTML = "Su peso es normal.";
+        document.getElementById("comentario").innerHTML = "Su peso es normal.";
     } 
     else if (IMC >= 25.0 && IMC <= 29.9) {
         document.getElementById("resultado").innerHTML = "Su índice de masa corporal es " + IMC.toFixed(2);
-        document.getElementById("recomendacion").innerHTML = "Su peso es superior al normal.";
+        document.getElementById("comentario").innerHTML = "Su peso es superior al normal.";
     } 
     else {
         document.getElementById("resultado").innerHTML = "Su índice de masa corporal es " + IMC.toFixed(2);
-        document.getElementById("recomendacion").innerHTML = "Su peso es de obesidad.";
+        document.getElementById("comentario").innerHTML = "Su peso es de obesidad.";
     }
 }
