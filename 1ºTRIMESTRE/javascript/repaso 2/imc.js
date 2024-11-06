ea4267701cea3878cb562cc867100af94c074040
mariadpf05 <mariadelpino05@iesamachado.org>
2024-11-06 12:31:36 +0100

    a

--- 1ºTRIMESTRE/javascript/repaso 2/imc.js
+++ 1ºTRIMESTRE/javascript/repaso 2/imc.js
@@ -13,19 +13,19 @@ function CalcularIMC(){
     var IMC = peso / (alturaMetros * alturaMetros);
 
     if (IMC < 18.5) {
-        document.getElementById("resultado").innerHTML = "Su índice de masa corporal es " + IMC.toFixed(2) + ". Esto indica: Peso inferior al normal.";
-        document.getElementById("recomendacion").innerHTML = "Es recomendable consultar con un nutricionista para ganar peso de forma saludable.";
+        document.getElementById("resultado").innerHTML = "Su índice de masa corporal es " + IMC.toFixed(2);
+        document.getElementById("recomendacion").innerHTML = "Su peso es inferior al normal.";
     } 
     else if (IMC >= 18.5 && IMC <= 24.9) {
-        document.getElementById("resultado").innerHTML = "Su índice de masa corporal es " + IMC.toFixed(2) + ". Esto indica: Peso normal.";
-        document.getElementById("recomendacion").innerHTML = "Mantenga una dieta balanceada y actividad física regular para conservar su salud.";
+        document.getElementById("resultado").innerHTML = "Su índice de masa corporal es " + IMC.toFixed(2);
+        document.getElementById("recomendacion").innerHTML = "Su peso es normal.";
     } 
     else if (IMC >= 25.0 && IMC <= 29.9) {
-        document.getElementById("resultado").innerHTML = "Su índice de masa corporal es " + IMC.toFixed(2) + ". Esto indica: Peso superior al normal.";
-        document.getElementById("recomendacion").innerHTML = "Considere realizar más actividad física y ajustar su dieta para reducir el peso.";
+        document.getElementById("resultado").innerHTML = "Su índice de masa corporal es " + IMC.toFixed(2);
+        document.getElementById("recomendacion").innerHTML = "Su peso es superior al normal.";
     } 
     else {
-        document.getElementById("resultado").innerHTML = "Su índice de masa corporal es " + IMC.toFixed(2) + ". Esto indica: Obesidad.";
-        document.getElementById("recomendacion").innerHTML = "Es recomendable acudir a un profesional de la salud para obtener ayuda en la pérdida de peso.";
+        document.getElementById("resultado").innerHTML = "Su índice de masa corporal es " + IMC.toFixed(2);
+        document.getElementById("recomendacion").innerHTML = "Su peso es de obesidad.";
     }
 }
