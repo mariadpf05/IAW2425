6573a48678653e5a786ec1a82f4b95a49d941d0a
mariadpf05 <mariadelpino05@iesamachado.org>
2024-11-13 11:49:57 +0100

    ejercicios

--- 1ºTRIMESTRE/jquery/ejercicios jquery/ej1.js
+++ 1ºTRIMESTRE/jquery/ejercicios jquery/ej1.js
@@ -1,1 +1,10 @@
 /* Rellena este fichero */
+$(document).ready(function(){
+    $("#btn-mostrar").click(function(){
+      $("p, h1").show();
+    });
+
+    $("#btn-ocultar").click(function(){
+      $("p, h1").hide();
+    });
+  });
