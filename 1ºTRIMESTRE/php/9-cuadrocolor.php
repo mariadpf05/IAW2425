86527b92b3974da795dcd87f104ba9412c04f800
mariadpf05 <mariadelpino05@iesamachado.org>
2024-12-02 13:15:12 +0100

    php

--- 1ºTRIMESTRE/php/9-cuadrocolor.php
+++ 1ºTRIMESTRE/php/9-cuadrocolor.php
@@ -7,10 +7,10 @@
 </head>
 <body> 
     <?php
-    $num1 =  rand(0, 255);
-    $num2 =  rand(0, 255);
-    $num3 =  rand(0, 255);
-    echo "<div style='background-color: rgb(". $num1. "," .$num2. "," .$num3  .");  width: 300px;height: 300px;'> </div>"
+    $num1 = rand(0, 255);
+    $num2 = rand(0, 255);
+    $num3 = rand(0, 255);
+    echo "<div style='background-color: rgb($num1, $num2, $num3); width: 300px; height: 300px;'></div>";
     ?>
 </body>
 </html>
