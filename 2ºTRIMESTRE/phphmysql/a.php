26f0409249ca8f613392565b9384e7ab20a0e751
mariadpf05 <mariadelpino05@iesamachado.org>
2025-01-13 14:16:14 +0100

    phphmysql

---
+++ 2ºTRIMESTRE/phphmysql/a.php
@@ -0,0 +1,13 @@
+<?php 
+    $servername = "sql308.thsite.top"; //Nombre del servidor
+    $username = "thsi_38097480"; //Nombre de usuario
+    $password =""; //Contraseña
+    //Crear conexión
+    $conn = new mysqli($servername, $username, $password);
+
+    //Comprueba la conexión
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+?>
